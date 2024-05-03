78ea13bd16d1bba10e58d7693d0135cda35a6d39
tamcon2 <164995447+tamcon2@users.noreply.github.com>
2024-05-03 17:12:09 +0700

    Update and rename stup.py to setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+from os.path import exists
+from setuptools import setup
+
+setup(name='kanren',
+      version='0.2.3',
+      description='Logic Programming in python',
+      url='http://github.com/logpy/logpy',
+      author='Matthew Rocklin',
+      author_email='mrocklin@gmail.com',
+      license='BSD',
+      packages=['kanren'],
+      install_requires=open('requirements.txt').read().split('\n'),
+      tests_require=[
+          'pytest',
+          'sympy'
+      ],
+      long_description=open('README.md').read() if exists("README.md") else "",
+      zip_safe=False,
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "License :: OSI Approved :: BSD License",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6",
+                   "Programming Language :: Python :: 3.7",
+                   "Programming Language :: Python :: Implementation :: CPython",
+                   "Programming Language :: Python :: Implementation :: PyPy",
+      ],
+)
